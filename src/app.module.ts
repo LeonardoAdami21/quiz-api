9df82934ad74ccf3823dcb23b065ecd01be30416
@leonardorossato21 <leonardorossato21@gmail.com>
2025-05-01 17:39:57 -0300

    feat/auth-and-auto, implementação dos modulos de usuarios e auth para realizar a autenticação e autorização da API.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtSecret } from './env/envoriment';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.register({
+      secret: jwtSecret,
+      signOptions: { expiresIn: '1h' },
+    }),
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
